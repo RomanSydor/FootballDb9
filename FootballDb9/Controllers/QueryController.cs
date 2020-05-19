49414c1f33802e1f17bb466ba3c9d81725f55edb
Roman <first.luker@gmail.com>
2020-05-19 17:54:25 +0300

    Added error tracking logic.

--- FootballDb9/Controllers/QueryController.cs
+++ FootballDb9/Controllers/QueryController.cs
@@ -1,11 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
-using System.Data;
-using System.IO;
-using System.Linq;
 using System.Text;
-using System.Web;
 using System.Web.Mvc;
 
 namespace FootballDb9.Controllers
@@ -15,56 +11,68 @@ namespace FootballDb9.Controllers
         public static List<List<string>> TableHeader = new List<List<string>>();
         public static List<List<string>> TableBody = new List<List<string>>();
         public static string Query;
-        
+
         // GET: Query
         public ActionResult Index(string query)
         {
-            string connectionString = "server=localhost;user=root;database=lab2db;port=3306;password=1313";
-            Query += query;
-            MySqlConnection connection = new MySqlConnection(connectionString);
-            connection.Open();
-            string sql = query;
-            MySqlCommand command = new MySqlCommand(sql, connection);
-            MySqlDataReader reader = command.ExecuteReader();
-
-            int fieldCount = reader.FieldCount;
-            object[] fields = new object[fieldCount];
-
-            List<string> rowsHeader = new List<string>();
-            //tableHeader = new List<List<string>>();
-
-            for (int i = 0; i < fieldCount; i++)
+            try
             {
-                rowsHeader.Add(reader.GetName(i));
-            }
-            TableHeader.Add(rowsHeader);
+                ViewBag.Header = null;
+                ViewBag.Body = null;
+                ViewBag.Error = null;
+                TableHeader = new List<List<string>>();
+                TableBody = new List<List<string>>();
 
-            ViewBag.Header = TableHeader;
+                string connectionString = "server=localhost;user=root;database=lab2db;port=3306;password=1313";
+                Query += query;
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                connection.Open();
+                string sql = query;
+                MySqlCommand command = new MySqlCommand(sql, connection);
+                MySqlDataReader reader = command.ExecuteReader();
 
-            List<string> rowsBody = new List<string>();
-            //tableBody = new List<List<string>>();
-            while (reader.Read())
-            {
+                int fieldCount = reader.FieldCount;
+                object[] fields = new object[fieldCount];
+
+                List<string> rowsHeader = new List<string>();
+
                 for (int i = 0; i < fieldCount; i++)
                 {
-                    fields[i] = reader[i];
-                    rowsBody.Add(Convert.ToString(fields[i]));
+                    rowsHeader.Add(reader.GetName(i));
                 }
-                TableBody.Add(rowsBody);
-                rowsBody = new List<string>();
+                TableHeader.Add(rowsHeader);
+
+                ViewBag.Header = TableHeader;
+
+                List<string> rowsBody = new List<string>();
+
+                while (reader.Read())
+                {
+                    for (int i = 0; i < fieldCount; i++)
+                    {
+                        fields[i] = reader[i];
+                        rowsBody.Add(Convert.ToString(fields[i]));
+                    }
+                    TableBody.Add(rowsBody);
+                    rowsBody = new List<string>();
+                }
+
+                ViewBag.Body = TableBody;
+
+                reader.Close();
             }
-
-            ViewBag.Body = TableBody;
-
-            reader.Close();
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex;
+            }
             return View();
         }
 
-        public void CSVSave() 
+        public void CSVSave()
         {
 
             var sb = new StringBuilder();
-            
+
             string qry = Query;
 
             foreach (var item in TableHeader)
